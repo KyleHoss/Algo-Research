59fc44870beec42e2f1ee5a39ce6ce6aba88e664
Kyle Hostetter <Kyle.Hostetter13@gmail.com>
2024-08-04 22:04:09 -0500

    Making corrections to functions in class!

--- OandaAPI.py
+++ OandaAPI.py
@@ -6,12 +6,64 @@ class OandaAPI:
         self.API_URL = "api-fxtrade.oanda.com"
         self.STREAM_URL = "stream-fxtrade.oanda.com"
 
+        self.granularity_conv_dict = {'S5': 5,'S10': 10,'S15': 15,'S30': 30,
+                                'M1': 60,'M2': 120,'M4': 240,'M5': 300,
+                                'M10': 600,'M15': 900,'M30': 1800,
+                                'H1': 3600,'H2': 7200,'H3': 10800,
+                                'H4': 14400,'H6': 21600,'H8': 28800,
+                                'H12': 43200,'D': 86400,'W': 604800,
+                                'M': 2620800} 
 
-    def get_historical_data(self, instrument, start_date, end_date, granularity='D' ,as_pandas=True):
-        # Must import Necessary Libraries
+    def candle_delta(self, start_date='08/09/2023', end_date='08/09/2024', granularity='M5') -> float:
+        """This will return the number of candles between start_date and end_date.
+
+        Args:
+            start_date: Date to start calculations.
+            end_date: Date if ending.
+            granularity: Price update frequency.
+
+        Returns:
+            float: Number of candle sticks between start_date and end_date.
+        """
+        # Import Necessary Libraries
+        import pandas as pd
+
+        # Convert Dates to dateTimes
+        start_datetime = pd.to_datetime(start_date)
+        end_datetime = pd.to_datetime(end_date)
+
+        # Calculate Seconds Between DateTimes
+        datetime_dif = end_datetime - start_datetime   
+        seconds_between = datetime_dif.total_seconds() 
+
+        # Convert second between to number of candles
+        num_candles = seconds_between / self.granularity_conv_dict.get(granularity)
+
+        # Return Number of Candle Sicks
+        return num_candles
+
+    def historical_data(self, currency_pair='EUR_USD', start_date='08/09/2023', end_date='08/09/2024', granularity='D', as_pandas=True):
+        """Will get the historical json data for a given currency pair.
+
+        Args:
+            currency_pair: Currency pair name. 
+            start_date: Date of start of historical data.
+            end_date: Closest date of historical data. 
+            granularity: How often price data is updated.
+            as_pandas: Set to True to return as pandas dataframe.
+
+        Returns:
+            json: Historical pricing data in json format.
+            Pandas: Historical pricing dataframe.
+        """
+         # Must import Necessary Libraries
         import time
         import requests
         import pandas as pd
+
+        print(self.ACCOUNT_ID)
+        print(self.API_URL)
+        print(self.API_TOKEN)
 
         # Convert Dates to DateTime objects
         start_dateTime = pd.to_datetime(start_date)
@@ -26,30 +78,27 @@ class OandaAPI:
 
         # Query Parameters
         parameters = {"from": str(unix_start), 
-                      "to": str(unix_end), 
-                      "granularity": granularity}
+                        "to": str(unix_end), 
+                        "granularity": granularity}
         
         # Historical Candle Stick Data URL
-        candle_url = f"/v3/accounts/{self.ACCOUNT_ID}/instruments/{instrument}/candles"
+        candle_url = f"/v3/accounts/{self.ACCOUNT_ID}/instruments/{currency_pair}/candles"
 
         # Request for Candle Stick Data
         json_response = requests.get("https://"+self.API_URL+candle_url, headers=auth_header, params=parameters).json()
+        print(json_response)
 
-        # Return as Pandas DataFrame
+        # Create Pandas DataFrame
         if as_pandas is True:
             # Convert Json to Pandas df
-            candle_df = pd.json_normalize(json_response['candles'])
+            candle_df = pd.json_normalize(json_response["candles"])
 
             # Drop complete Column
             candle_df.drop('complete', inplace=True, axis=1)
 
             # Rename Columns
-            candle_df.rename(inplace=True,columns={'time':'DateTime',
-                                                   'volume':'Volume',
-                                                   'mid.o':'Open',
-                                                   'mid.h':'High',
-                                                   'mid.l':'Low',
-                                                   'mid.c':'Close'})
+            candle_df.rename(inplace=True,columns={'time':'DateTime','volume':'Volume','mid.o':'Open',
+                                                    'mid.h':'High','mid.l':'Low','mid.c':'Close'})
 
             # Convert Datetime= Column to datetime object
             candle_df['DateTime'] = pd.to_datetime(candle_df['DateTime'])
@@ -59,13 +108,99 @@ class OandaAPI:
 
             # Restructure Column Names
             candle_df = candle_df[['Open','High','Low','Close','Volume']]
-
+            
+            # Return DataFrame
             return candle_df
+    
+        # Return as Json 
         else: 
             return json_response
 
+    def load_candlestick_datafame(self, currency_pair='EUR_USD',start_date='08/09/2023', end_date='08/09/2024', granularity='D'):
+        """This will return a large candle stick dataframe 
 
-    def show_granularity(self):
+        Args:
+            currency_pair: Currency pair name. 
+            start_date: Date of start of historical data.
+            end_date: Closest date of historical data. 
+            granularity: How often price data is updated.
+
+        Returns:
+            Pandas: A large dataframe of candle stick data.
+        """
+
+        # Import Necessary Libraries
+        import math
+        import tqdm
+        import pandas as pd
+        from datetime import timedelta
+
+        # Set Constants
+        MAX_CANDLES = 4000  # Max Number of candles retrieved each call
+        
+        # Calculate number of candles between datetimes
+        num_candles = self.candle_delta(start_date=start_date,end_date=end_date,
+                                        granularity=granularity)
+
+        # Convert to DateTime foramt
+        start_datetime = pd.to_datetime(start_date)
+        end_datetime = pd.to_datetime(end_date)
+
+        # Initialize Variables
+        candle_secs = MAX_CANDLES * math.floor(self.granularity_conv_dict.get(granularity))   # Max candles forwards in seconds
+        max_date = start_datetime+timedelta(seconds=candle_secs)                              # Max date forwards in candles
+        new_start_datetime = start_datetime                                                   # New Start DateTime variable to hold future start date
+        candles_df = pd.DataFrame()                                                           # Pandas DataFrame to hold candlestick data
+        start_datetime_list = []                                                              # Start DateTime list 
+        end_datetime_list = []                                                                # End DateTime list
+
+        if num_candles < MAX_CANDLES:
+
+            # Fetch Historical Price Data
+            candles_df = self.historical_data(currency_pair=currency_pair, start_date=start_date,
+                                             end_date=end_date, granularity=granularity,
+                                             as_pandas=True)
+            
+            # Return Candle Stick DataFrame
+            return candles_df
+        
+        else: 
+            while True:
+                if max_date >= end_datetime:
+
+                    # Append Last DateTime values
+                    start_datetime_list.append(new_start_datetime)
+                    end_datetime_list.append(end_datetime)
+
+                    # Break out of While Loop
+                    break
+
+                else:
+                    # Append DateTimes to lists
+                    start_datetime_list.append(new_start_dateTime)
+                    end_datetime_list.append(max_date) 
+                    
+                    # Overwrite existing variables
+                    candle_secs = MAX_CANDLES * math.floor(self.granularity_conv_dict.get(granularity))   # Max candles forwards in seconds
+                    new_start_dateTime = max_date+timedelta(seconds=candle_secs)                          # Set new date time to old max_date plus additional candle secs
+                    max_date = new_start_dateTime+timedelta(seconds=candle_secs)                          # Set new max date to last start datetime plus additional candle secs
+
+            # Create Loading Bar 
+            for i in tqdm.tqdm(range(len(start_datetime_list)),                           # Iteration range
+                               colour='GREEN',                                            # Loading Bar Color
+                               ncols=100,                                                 # Loading Bar width
+                               desc=f'Downloading {currency_pair} {granularity} Data'):   # Loading Message
+
+                # Download Historical Data
+                new_candles_df = self.historical_data(currency_pair=currency_pair, start_date=start_datetime_list[i],   
+                                                      end_date=end_datetime_list[i], granularity=granularity)
+                # Concatenate DateFrames
+                candles_df = pd.concat([candles_df, new_candles_df])
+            
+            # Return Candles Stick DataFrame
+            return candles_df
+            
+    def show_granularity(self)-> dict:
         import pandas as pd
 
         # Values of Granularity
